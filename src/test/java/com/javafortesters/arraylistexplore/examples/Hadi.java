57b70c6f91b42eb0f22dac0364e8ccedcd6f8966
Waqas Raza <waqas.raza@sedexglobal.com>
2018-11-12 23:08:24 +0000

    chap009 array exercises part 1

--- src/test/java/com/javafortesters/arraylistexplore/examples/Hadi.java
+++ src/test/java/com/javafortesters/arraylistexplore/examples/Hadi.java
@@ -3,6 +3,6 @@ package com.javafortesters.arraylistexplore.examples;
 public class Hadi {
 
     public static void main (String[] args) {
-        System.out.println("DINOSOUR");
+        System.out.println("ROBOT");
     }
 }
--- src/test/java/com/javafortesters/chap009arraysandforloopiteration/examples/ArrayTest.java
+++ src/test/java/com/javafortesters/chap009arraysandforloopiteration/examples/ArrayTest.java
@@ -4,6 +4,8 @@ import com.javafortesters.domainentities.User;
 import org.junit.Test;
 
 
+import java.util.Arrays;
+
 import static org.junit.Assert.assertEquals;
 
 public class ArrayTest {
@@ -19,5 +21,87 @@ public class ArrayTest {
         assertEquals("bob", users[0].getUsername());
         assertEquals("eris", users[1].getUsername());
         assertEquals("ken", users[2].getUsername());
+        String alias = "";
+
+        for (User user : users) {
+            alias = user.getUsername();
+            System.out.println(alias);
+
+        }
+    }
+
+    @Test
+    public void arrayWorkdayTest() {
+        String[] workdays = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+        String days = "";
+        for (String workday : workdays) {
+            days = days + "|" + workday;
+        }
+
+        assertEquals("|Monday|Tuesday|Wednesday|Thursday|Friday", days);
+    }
+
+    @Test
+    public void traditionalForLoop() {
+        String[] workdays = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+        String days = "";
+
+        for (int i = 0; i < workdays.length; i++) {
+            days = days + "|" + i + "-" + workdays[i];
+        }
+
+        assertEquals("|0-Monday|1-Tuesday|2-Wednesday|3-Thursday|4-Friday", days);
+    }
+
+    @Test
+    public void createAnArrayOf100Users() {
+        User[] users = new User[100];
+        for (int userIndex = 0; userIndex < users.length; userIndex++) {
+            int userId = userIndex + 1;
+            users[userIndex] = new User("user" + userId, "password" + userId);
+        }
+        assertEquals(100, users.length);
+
+        for (User aUser : users) {
+            System.out.println(aUser.getUsername() + "," + aUser.getPassword());
+        }
+
+        int userID = 1;
+        for (User aUser : users) {
+            assertEquals("user" + userID, aUser.getUsername());
+            assertEquals("password" + userID, aUser.getPassword());
+            userID++;
+        }
+
+        assertEquals(101, userID);
+
+    }
+
+    @Test
+    public void sortTheArray() {
+
+        String[] workdays = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+
+        Arrays.sort(workdays);
+
+        assertEquals("Friday", workdays[0]);
+        assertEquals("Monday", workdays[1]);
+        assertEquals("Thursday", workdays[2]);
+        assertEquals("Tuesday", workdays[3]);
+        assertEquals("Wednesday", workdays[4]);
+    }
+
+    @Test
+    public void sortDifferentCaseArray() {
+
+        String[] workdays = {"monday", "Tuesday", "Wednesday", "thursday", "Friday"};
+
+        Arrays.sort(workdays);
+
+        assertEquals("Friday", workdays[0]);
+        assertEquals("Tuesday", workdays[1]);
+        assertEquals("Wednesday", workdays[2]);
+        assertEquals("monday", workdays[3]);
+        assertEquals("thursday", workdays[4]);
     }
 }
